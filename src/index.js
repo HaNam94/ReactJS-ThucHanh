f78b3c422c66308f02577bd65152c69932c55f9f
HaNam94 <haduynam.94@gmail.com>
2024-07-17 15:13:00 +0700

    Thuc Hanh React

--- src/index.js
+++ src/index.js
@@ -1,17 +1,43 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+//--BaiThucHanh1--
+//const name = "Your name";
+//React.createElement("h1",{style: {textAlign: "center"}}, name)
+//const root = ReactDOM.createRoot(document.getElementById("root"));
+//root.render(
+//    React.createElement("h1", { style: { textAlign: "center" } }, name)
+//);
+
+
+//--BaiThucHanh2--
+//const name = "My name is Nam"
+//const root = ReactDOM.createRoot(document.getElementById("root"));
+//root.render(<h1 style={{textAlign: "center"}}>{name}</h1>);
+
+
+//--BaiThucHanh3--
+//const fruit = ["Apple", "Banana", "Orange", "Apricot", "Black rowan", "Cranberry"];
+//const root = ReactDOM.createRoot(document.getElementById("root"));
+//root.render(
+//  <div>
+//      <h1>List of fruits</h1>
+//      <ul>
+//          {fruit.map(item =>(<li>{item}</li>))}
+//      </ul>
+//  </div>
+// );
+
+
+//--BaiThucHanh4--
+const root = ReactDOM.createRoot(document.getElementById("root"));
+const tick = () => {
+    root.render(
+        <div>
+            <h1>Hello, world!</h1>
+            <h2>It is {new Date().toLocaleTimeString()}.</h2>
+        </div>
+    );
+};
+    setInterval(tick, 1000);
